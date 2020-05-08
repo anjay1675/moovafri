9129943d04623f56d600f494f305253c0e090911
anjabaatiku1675 <atikuajabadachi@gmail.com>
2020-05-08 17:41:04 +0100

    test run 2

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -3,33 +3,33 @@ const nodemailer = require("nodemailer");
 const Router = require('express').Router();
 
 
+// in order to use gmail as a transporter smtp make sure you switch on less secure apps authentication for the account: link bellow
+// https://myaccount.google.com/lesssecureapps
 
 Router.route("/").post((req, res)=>{
     const email = req.body.email;
     const name = req.body.name;
+    const subject = req.body.subject;
     const message = req.body.message;
-    // in order to use gmail as a transporter smtp make sure you switch on less secure apps authentication for the account: link bellow
-        // https://myaccount.google.com/lesssecureapps
     let transport = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
+        host: 'smtp.gmail.com.',
         port: 587,
         secure: false,
         requireTLS: true,
         auth: {
-    
-            user: 'atikuanjabadachi@gmail.com',
-            pass: 'chinban1675'
+            user: 'moovafrica.com@gmail.com',
+            pass: 'officesecret'
         }
-        });
+    });
     const mailInfo = {
         from: email,
-        to: 'atikuanjabadachi@gmail.com',
-        subject: 'Testing Anjay project',
+        to: 'anjaba.atiku@moovafrica.com',
+        subject: subject,
         html: `<h1>Mail from ${name} </h1><p> ${message} </p>`
     };
     transport.sendMail(mailInfo, function(err, info) {
         if(!err){
-            res.status(200).send("Message sent successfully")
+            res.status(200).json({status:'success', msg:`Message Sent Successfully`})
         }else{
             res.status(400).json("Error: "+err)
         }
