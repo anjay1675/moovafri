68e1654a4b29c0bbe65b1bcb57259732362aefdf
anjabaatiku1675 <atikuajabadachi@gmail.com>
2020-05-10 04:28:03 +0100

    nice

--- src/components/Deals.js
+++ src/components/Deals.js
@@ -6,7 +6,6 @@ import {Navbar, Nav} from 'react-bootstrap'
 
 const Deals =()=>{
     return(
-        
         <div>
             <div className="bar">
             <div className="bar1">
@@ -18,7 +17,7 @@ const Deals =()=>{
         <Navbar collapseOnSelect expand="sm">
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">    
-                <img className="logo" src="./images/w1.png" alt="picture" width="180px" height="80px"/>
+                <img className="logo" src="./images/w1.png" alt="Moov" width="180px" height="80px"/>
             <Nav className="mr-auto">    
                 <Link className="nav-link" to='/'>Home</Link>
                 <Link className="nav-link" to='/cargo'>Cargo Delivery</Link>
